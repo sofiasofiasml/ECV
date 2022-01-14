ed17f3ed93cff144b7f1911505148c4c7868012f
Sofia Marlop <romata1966@gmail.com>
2022-01-14 20:35:37 +0100

    fuera jquery

--- code.js
+++ code.js
@@ -1,30 +1,43 @@
-
+var input = document.querySelector("input");
+var state = document.querySelector("#horizontal_top aside#state");
 function sendMissage()
 {
-    var input = document.querySelector("input");
-    var elem = document.createElement("p");
+    var elem = document.createElement("p");  //div
     elem.className = "from-me"; 
     elem.innerText = input.value; 
     var chat = document.querySelector("#chat");
-    chat.appendChild(elem)
+    chat.appendChild(elem); 
     input.value=" "; 
+    chat.scrollTop = 1000000; 
 }
 
 /*Change Status. Role of https://stackoverflow.com/questions/12797700/jquery-detect-change-in-input-field*/
 const typer = document.getElementById('myTextbox1');
-const status = document.getElementById('state');
 let timer, timeoutVal = 300; // time it takes to wait for user to stop typing in ms
 /*When you write in input, change state: Escribiendo*/
-$('#myTextbox1').on('input', function() {
-    status.textContent="Escribiendo...";
-});
 
-typer.addEventListener('keyup', handleKeyUp);
+text= document.querySelector('#myTextbox1'); 
+
+input.addEventListener("keydown", OnKeyPress); 
+/*Parentesis le pasas el resultado de la función, no la función*/
+typer.addEventListener('keyup', handleKeyUp); /*Eventos: cuando sucede algo */
 /* You stop writing. Role of https://dev.to/eaich/how-to-detect-when-the-user-stops-typing-3cm1*/
 function handleKeyUp() {
     window.clearTimeout(timer); // prevent errant multiple timeouts from being generated
     timer = window.setTimeout(() => {
-    status.innerHTML = 'En linea';
+    state.innerHTML = 'En linea';
 }, timeoutVal);
 }
+
+function OnKeyPress(e)
+{
+    if(e.code == "Enter")
+    {
+        sendMissage(); 
+    }
+
+    state.textContent="Escribiendo...";
+        
+}
+
 /*Finish Change Status*/
